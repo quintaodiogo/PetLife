ff300b74f77decdcd44d1786d04e794649549d2c
Diogo de Paula <quintaodiogo@gmail.com>
2024-07-07 10:39:50 -0300

    Melhoria no menu-mobile

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,26 @@
 let openMenuButtons = document.querySelectorAll(".menu-toggle");
 let mobileMenu = document.querySelector(".main-nav-mobile");
+let links = document.querySelectorAll(".links");
+
+function toggleMenu() {
+    openMenuButtons.forEach(button => {
+        if (button.classList.contains("open")) {
+            button.classList.remove("open");
+            mobileMenu.classList.remove("open");
+        } else {
+            button.classList.add("open");
+            mobileMenu.classList.add("open");
+        }
+    });
+}
 
 openMenuButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    if (this.classList.contains("open")) {
-      this.classList.remove("open");
-      mobileMenu.classList.remove("open");
-    } else {
-      this.classList.add("open");
-      mobileMenu.classList.add("open");
-    }
+  button.addEventListener("click", toggleMenu);
+});
+
+links.forEach(function (link) {
+  link.addEventListener("click", function () {
+    openMenuButtons.forEach(button => button.classList.remove("open"));
+    mobileMenu.classList.remove("open");
   });
 });
